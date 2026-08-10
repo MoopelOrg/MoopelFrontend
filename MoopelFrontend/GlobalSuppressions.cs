808db1f507d205f3b17a49ed0b87f55ccb071697
Moopel <email@derekgsmith.com>
2026-08-09 21:14:19 -0400

    Add base files

---
+++ MoopelFrontend/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "CA1873:Avoid potentially expensive logging", Justification = "Logging isn't disabled")]
+[assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "I don't like primary constructors")]
