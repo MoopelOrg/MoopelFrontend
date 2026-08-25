7e874bfed19568f69bfcb0ecd3af936ff68c8ffc
Moopel <email@derekgsmith.com>
2026-08-25 18:31:08 -0400

    Remove double / from route

--- MoopelFrontend.Tests/E2E/NavigationTests.cs
+++ MoopelFrontend.Tests/E2E/NavigationTests.cs
@@ -58,14 +58,14 @@ public sealed class NavigationTests
     public async Task HomePage_RedirectsAnonymousUserToLogin()
     {
         await _page.GotoAsync($"{_host.BaseUrl}/");
-        await Assertions.Expect(_page).ToHaveURLAsync($"{_host.BaseUrl}/{PageRoutes.Login}");
+        await Assertions.Expect(_page).ToHaveURLAsync($"{_host.BaseUrl}{PageRoutes.Login}");
         await Assertions.Expect(_page.Locator("h1")).ToHaveTextAsync("Log in");
     }
 
     [Test]
     public async Task LoginPage_ShowsCredentialsForm()
     {
-        await _page.GotoAsync($"{_host.BaseUrl}/{PageRoutes.Login}");
+        await _page.GotoAsync($"{_host.BaseUrl}{PageRoutes.Login}");
         await Assertions.Expect(_page.Locator("#username")).ToBeVisibleAsync();
         await Assertions.Expect(_page.Locator("#password")).ToBeVisibleAsync();
     }
